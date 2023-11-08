eb6e831ac27541af80cdf22a070582bfbc15b37a
chimenemking <chimenemwillis@gmail.com>
2023-11-07 17:01:58 -0800

    added readme and authors file

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple shell project in ALX that is done by Chimenem and Wendu
---
+++ shell.h
@@ -0,0 +1,10 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+
+#endif /* my simple shell */
