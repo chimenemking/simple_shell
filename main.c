feaf9a25068812a63562febad171aa950728355e
chimenemking <87863371+chimenemking@users.noreply.github.com>
2023-11-14 21:40:12 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 /**
  * main - The main function that runs the shell.
  */
-int main(int argc, char *argv[], char **env) {
-
+int main(int argc, char *argv[], char **env)
+{
   char *command_buffer = NULL;
   char *input_prompt = "$ ";
   ssize_t bytes_read;
-  int child_pid;
+  int chd_pid;
   bool input_from_pipe = false;
   struct stat command_info;
 
@@ -34,13 +34,14 @@ int main(int argc, char *argv[], char **env) {
     }
 
     /* you now execute the command in the child process */
-    if (chd_pid == 0) {
+    if (chd_pid == 0)
+    {
       execute_command(command_buffer);
       exit(EXIT_SUCCESS);
     }
 
     /* wait for child to exit */
-    wait_for_child_process(child_pid);
+    wait_for_child_process(chd_pid);
   }
 
   /* Free up the command buffer */
@@ -50,13 +51,15 @@ int main(int argc, char *argv[], char **env) {
   return (0);
 }
 
-int wait_for_child_process(int pid) {
-  // Wait for the child process to exit and get its status
+int wait_for_child_process(int pid)
+{
+  /* Wait for child process to exit & get its status */
   int wstatus;
   int wait_result = waitpid(pid, &wstatus, 0);
 
-  // Check for errors
-  if (wait_result == -1) {
+  /* Check for errors here */
+  if (wait_result == -1)
+  {
     perror("Error (wait)");
     exit(EXIT_FAILURE);
   }
